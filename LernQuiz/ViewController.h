01d13c25b9486efbe7e388198a3d8d79f9088800
engelskirchen <wannyalchemic@gmail.com>
2014-05-05 14:50:22 +0200

    Initial Commit

---
+++ LernQuiz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LernQuiz
+//
+//  Created by Schueler on 05.05.14.
+//  Copyright (c) 2014 TommyLarsWassilij. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
